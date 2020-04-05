f088420b5c5b89b3098fd31d0ea5583099903e0a
Levente Győző Lénárt <leviiibog@gmail.com>
2020-04-05 23:38:57 +0200

    debug

--- bin/auth.sh
+++ bin/auth.sh
@@ -40,6 +40,7 @@ echo "$LICENSE" | tr -d '\r' > "/Library/Application Support/Unity/Unity_lic.ulf
     -quit \
     -batchmode \
     -createManualActivationFile
+    -nographics
     -username "$UNITY_USERNAME" \
     -password "$UNITY_PASSWORD" \
     -logfile /dev/stdout || true
